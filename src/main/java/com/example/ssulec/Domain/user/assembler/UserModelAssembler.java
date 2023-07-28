296d23bfe5725c1ead62d9b08362b0894a78e3cf
leeminseo0923 <64004511+leeminseo0923@users.noreply.github.com>
2023-07-28 15:42:46 +0900

    feat: Restful API 용으로 hateoas사용하는 코드로 변경

---
+++ src/main/java/com/example/ssulec/Domain/user/assembler/UserModelAssembler.java
@@ -0,0 +1,10 @@
+package com.example.ssulec.Domain.user.assembler;
+
+import com.example.ssulec.Domain.user.domain.User;
+import org.springframework.hateoas.EntityModel;
+
+public class UserAssembler {
+    public static EntityModel<User> of(User user) {
+        
+    }
+}
--- src/main/java/com/example/ssulec/Domain/user/controller/UserController.java
+++ src/main/java/com/example/ssulec/Domain/user/controller/UserController.java
@@ -1,28 +1,31 @@
 package com.example.ssulec.Domain.user.controller;
 
+import com.example.ssulec.Domain.user.assembler.UserModelAssembler;
 import com.example.ssulec.Domain.user.domain.User;
 import com.example.ssulec.Domain.user.exception.UserNotFoundException;
 import com.example.ssulec.Domain.user.repository.UserRepository;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
 @RestController
 public class UserController {
     UserRepository userRepository;
+    UserModelAssembler userModelAssembler;
 
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @GetMapping("/user/{id}")
-    User one(@PathVariable Long id) {
-        return userRepository.findById(id).orElseThrow(
+    public EntityModel<User> one(@PathVariable Long id) {
+        User user = userRepository.findById(id).orElseThrow(
                 () -> new UserNotFoundException("User not found")
         );
+        return userModelAssembler.toModel(user);
     }
 }
