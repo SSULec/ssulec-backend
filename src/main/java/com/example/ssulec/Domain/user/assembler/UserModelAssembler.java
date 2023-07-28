2f18181e52582dff268495ac509b31ba2129b4ad
leeminseo0923 <64004511+leeminseo0923@users.noreply.github.com>
2023-07-28 15:46:47 +0900

    feat: User를 EntityModel로 변환해 줄 UserModelAssembler 추가

--- src/main/java/com/example/ssulec/Domain/user/assembler/UserModelAssembler.java
+++ src/main/java/com/example/ssulec/Domain/user/assembler/UserModelAssembler.java
@@ -1,10 +1,19 @@
 package com.example.ssulec.Domain.user.assembler;
 
+import com.example.ssulec.Domain.user.controller.UserController;
 import com.example.ssulec.Domain.user.domain.User;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
 
-public class UserAssembler {
-    public static EntityModel<User> of(User user) {
-        
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+public class UserModelAssembler implements RepresentationModelAssembler<User, EntityModel<User>> {
+
+    @Override
+    public EntityModel<User> toModel(User user) {
+        return EntityModel.of(user,
+                linkTo(methodOn(UserController.class).one(user.getId())).withSelfRel()
+                );
     }
 }
