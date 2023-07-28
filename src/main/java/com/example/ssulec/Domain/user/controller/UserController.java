85858b764741f706db5dcc0b370bf019f55370ae
leeminseo0923 <64004511+leeminseo0923@users.noreply.github.com>
2023-07-28 15:49:56 +0900

    docs: 미사용 import문 제거

--- src/main/java/com/example/ssulec/Domain/user/controller/UserController.java
+++ src/main/java/com/example/ssulec/Domain/user/controller/UserController.java
@@ -4,10 +4,7 @@ import com.example.ssulec.Domain.user.assembler.UserModelAssembler;
 import com.example.ssulec.Domain.user.domain.User;
 import com.example.ssulec.Domain.user.exception.UserNotFoundException;
 import com.example.ssulec.Domain.user.repository.UserRepository;
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +19,7 @@ public class UserController {
     }
 
     @GetMapping("/user/{id}")
-    public EntityModel<User> one(@PathVariable Long id) {
+    public EntityModel<?> one(@PathVariable Long id) {
         User user = userRepository.findById(id).orElseThrow(
                 () -> new UserNotFoundException("User not found")
         );
